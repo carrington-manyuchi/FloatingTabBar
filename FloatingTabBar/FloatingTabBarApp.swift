1b28099460a62b029c342dc1bbf384ddf965f73e
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-04-06 09:51:19 +0200

    Initial Commit

---
+++ FloatingTabBar/FloatingTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingTabBarApp.swift
+//  FloatingTabBar
+//
+//  Created by Manyuchi, Carrington C on 2025/04/06.
+//
+
+import SwiftUI
+
+@main
+struct FloatingTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
